5c5caebe33bee5c112260a117f0bf0889cb26544
Gabriel Mattias Antunes <g281199@dac.unicamp.br>
2026-09-17 15:35:48 -0300

    feat(auditoria): exporta o log de auditoria na API do pacote

--- src/votacao/__init__.py
+++ src/votacao/__init__.py
@@ -1,5 +1,12 @@
 """Sistema de votação digital com motor de regras extensível."""
 
+from votacao.auditoria import (
+    HASH_GENESE,
+    LogDeAuditoria,
+    RegistroAuditoria,
+    ResultadoVerificacao,
+    TipoEvento,
+)
 from votacao.modelos import (
     CasaLegislativa,
     ResultadoApuracao,
@@ -17,15 +24,20 @@ from votacao.regras.congresso import RegraCongresso
 __version__ = "0.1.0"
 
 __all__ = [
+    "HASH_GENESE",
     "CasaLegislativa",
+    "LogDeAuditoria",
     "MotorApuracao",
+    "RegistroAuditoria",
     "RegraAssembleia",
     "RegraCA",
     "RegraCongresso",
     "RegraDeVotacao",
     "ResultadoApuracao",
+    "ResultadoVerificacao",
     "StatusResultado",
     "TipoDecisaoAssembleia",
+    "TipoEvento",
     "TipoMateriaCongresso",
     "Voto",
     "__version__",
